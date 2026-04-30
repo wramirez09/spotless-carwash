24e29740e4a93c0efdc1ca9117ea5f70e7e05022
Will Ramirez <wramirez1980@gmail.com>
2026-04-29 21:39:34 -0500

    chore: fix text

--- src/components/HowClient.tsx
+++ src/components/HowClient.tsx
@@ -67,11 +67,11 @@ export default function HowClient({ data }: { data: HowData }) {
             </div>
           </div>
           <div
-            className="relative bg-[#06122c] rounded-3xl border-2 border-[#142855] p-9 flex flex-col items-center"
+            className="relative bg-[#06122c] rounded-3xl border-2 border-[#142855] px-9 pt-6 pb-5 flex flex-col items-center gap-4"
             style={{ boxShadow: 'inset 0 0 60px rgba(0,0,0,.6),0 30px 80px rgba(0,0,0,.5)' }}
           >
             <div className="display text-white text-lg">{data.bayLabel}</div>
-            <div className="flex-1 flex flex-col items-center justify-center gap-4.5 my-6">
+            <div className="flex flex-col items-center gap-4">
               <div className={'lamp' + (s.r ? ' on-r' : '')}></div>
               <div className={'lamp' + (s.y ? ' on-y' : '')}></div>
               <div className={'lamp' + (s.g ? ' on-g' : '')}></div>
